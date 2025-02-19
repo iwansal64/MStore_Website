b5f6705a9b1c45d9d7001337608dee2db174f370
iwansal64 <iwansal64@gmail.com>
2025-02-19 11:13:11 +0700

    Change google button to straight to the google account options

--- src/pages/components/googleLogin/googleLogin.jsx
+++ src/pages/components/googleLogin/googleLogin.jsx
@@ -5,7 +5,7 @@ export default function GoogleButton() {
     return <button
       type="button"
       className="w-full px-3 py-3 rounded-lg bg-white text-black hover:bg-white/60 duration-300 flex flex-row items-center gap-4 justify-center"
-      onClick={signIn}
+      onClick={() => { signIn("google") }}
     >
       <img src="/googleLogo.svg" className="w-6" alt="" />
       Google
