f2cb8deab466abe81c61b13698e42db056172cef
Iwansal64 <iwansal64@gmail.com>
2025-03-19 11:21:12 +0700

    Prevent double fetch to login API

--- src/pages/components/loginComponents/loginTrigger.jsx
+++ src/pages/components/loginComponents/loginTrigger.jsx
@@ -5,10 +5,13 @@ import { useSession } from "next-auth/react";
 export default function LoginTrigger() {
   const searchParams = useSearchParams();
   const session = useSession();
+  const is_logging_in = localStorage.getItem("is_logging_in");
 
   const is_login_param_exist = searchParams.has("login");
-  if(is_login_param_exist && session.data) {
+  if(is_login_param_exist && session.data && !is_logging_in) {
+    localStorage.setItem("is_logging_in", "yes");
     getLoginToken({ auth_data_token: session.data.access_token }).then(result => {
+      localStorage.removeItem("is_logging_in");
       if(result.success) {
         if(result.is_admin) {
             console.log("SUCCESSFULLY LOGIN AS ADMIN");
