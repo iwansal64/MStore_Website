6dc36118b0ad33f979378fd3fac325988ca2658a
Iwansal64 <iwansal64@gmail.com>
2025-03-23 23:53:40 +0700

    Add Loader (needed for notification & other pages)

---
+++ src/pages/components/loader/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./loader_style.css";
+
+const Loader = () => {
+    return <div className="w-full h-full flex justify-center items-center"><div className="loader"></div></div>
+}
+
+export default Loader;
